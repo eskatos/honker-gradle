0e83a36f9c1b992874cd15e5b6df97f55e60d39d
Paul Merlin <paul@nosphere.org>
2015-07-01 15:49:45 +0200

    Fix runtime classpath of the plugin

--- src/test/groovy/org/nosphere/honker/HonkerPluginIntegrationSpec.groovy
+++ src/test/groovy/org/nosphere/honker/HonkerPluginIntegrationSpec.groovy
@@ -38,7 +38,7 @@ class HonkerPluginIntegrationSpec extends IntegrationSpec {
                     output.dir( honkerGenNotice.outputDir, builtBy: honkerGenNotice )
                 }
             }
-            honkerGenNotice { footer = 'This product includes software developed at\nThe Apache Software Foundation (http://www.apache.org/).\n' }
+            honkerGenNotice { footer = 'This product includes software developed at\\nThe Apache Software Foundation (http://www.apache.org/).\\n' }
             repositories { jcenter() }
             dependencies {
 
@@ -85,7 +85,7 @@ class HonkerPluginIntegrationSpec extends IntegrationSpec {
         wasExecuted('honkerCheck')
         result.standardError.contains('Execution failed for task \':honkerCheck\'.')
         result.standardError.contains('License check failures: 2')
-        result.standardError.contains('mysql:mysql-connector-java:5.1.35:jar \'GPL\' licensed conflicts with the \'Apache 2\' license')
+        result.standardError.contains('mysql:mysql-connector-java:5.1.35:jar \'GNU General Public License\' licensed conflicts with the \'The Apache Software License, Version 2.0\' license')
         result.standardError.contains('org.antlr:stringtemplate:3.2.1:jar licensing data not found')
     }
 
